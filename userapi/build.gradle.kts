38d0a978b84741a86b9b7a5854150c00b39a52af
Pedro Luis <pedro.luisabj@gmail.com>
2021-06-30 10:33:55 -0300

    adiciona configuracoes iniciais do servidor de configuracao

--- userapi/build.gradle.kts
+++ userapi/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
