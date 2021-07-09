edd769c56f7e9b5d2f6fecc32583d2e3d81f51f2
Pedro Luis <pedro.luisabj@gmail.com>
2021-07-09 13:48:20 -0300

    Adiciona configuracoes para funcionamento do Cloud Bus com RabbitMQ para atualizar propriedades nas aplicacoes em producao

--- userapi/build.gradle.kts
+++ userapi/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+    implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
